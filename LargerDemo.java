ea1241564adbf5e48ae0a186e3fbb2d2229de32b
lfeng4782 <phyflx@hotmail.com>
2018-05-24 10:55:21 -0400

    firstTry

---
+++ LargerDemo.java
@@ -0,0 +1,14 @@
+public class LargerDemo {
+	public static int Larger (int x, int y){
+		if(x>y){
+			return x;
+		}else{
+			return y;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Larger(-38,23));	
+		// System.out.println("test");		
+	}	
+	}
